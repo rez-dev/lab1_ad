15f7adffcd309664842c50c0000ec69003fb5355
Angel Avendaño <angel.avendano@gmail.com>
2023-05-03 22:09:50 -0400

    lab terminado

--- bank-additional/Entrega1_analisis.R
+++ bank-additional/Entrega1_analisis.R
@@ -39,6 +39,8 @@ library(caret)
 library(leaps)
 library(car)
 library(mice)
+library(MASS)
+library(caret)
 
 # Importar datos
 poblacion <- read.csv2(file.choose(new = FALSE), encoding="utf8")
@@ -59,8 +61,6 @@ poblacion2 <- select(poblacion2, -pdays)
 
 sum(poblacion$poutcome == 'nonexistent')
 
-set.seed(123)
-cor(poblacion2, method = "kendall")
 
 
 
@@ -119,6 +119,37 @@ barplot(conteos_ordenados, col = colores, main = "Niveles de educación", xlab =
 #1    A     2.5
 #2    B     3.7
 #3    C     7.0
+
+
+##################
+# Caso job #
+##################
+
+# Contar el número de instancias en cada categoría de la variable "job"
+job_table <- table(poblacion2$job)
+
+# Convertir la salida en un data frame
+job_counts <- data.frame(job = names(job_table), count = as.numeric(job_table))
+
+# Ordenar el data frame por el total de instancias de forma descendente
+job_counts <- job_counts[order(-job_counts$count), ]
+
+# Agregar una columna con el porcentaje de instancias para cada categoría
+job_counts$percent <- job_counts$count / sum(job_counts$count) * 100
+
+# Calcular el promedio de la variable "y" para cada categoría de la variable "job"
+poblacion2$y <- ifelse(poblacion2$y == "yes", 1, 0)
+y_means_e <- aggregate(poblacion2$y, by = list(poblacion2$job), FUN = mean)
+names(y_means_e) <- c("job", "y_mean")
+
+# Unir las tablas de conteo y promedio por categoría de "job"
+job_counts <- merge(job_counts, y_means_e, by = "job")
+
+# Ordenar
+job_counts <- job_counts[order(job_counts$y_mean),]
+
+
+
 
 
 ##################
@@ -299,42 +330,8 @@ poutcome_counts <- merge(poutcome_counts, y_means_d, by = "poutcome")
 poutcome_counts <- poutcome_counts[order(poutcome_counts$y_mean),]
 
 
-
-
-
-########################################################################################################
-#          SECCION PARA CASO DE LOAN
-########################################################################################################
-# 
-# 
-# # Convertir la variable categórica a un factor para incluir todas las categorías
-# poblacion3[, "loan"] <- factor(poblacion3[, "loan"])
-# 
-# # Calcular la media de la variable numérica para cada categoría de la variable categórica
-# mean_vals <- aggregate(poblacion3[, "y"], list(poblacion3[, "loan"]), mean)
-# 
-# # Ordenar las categorías de la variable categórica en función de la media de la variable numérica
-# mean_vals_sorted <- mean_vals[order(mean_vals$x, decreasing = TRUE), ]
-# 
-# # Crea los indices crecientes para asignar los indices a las variables categoricas
-# mean_vals_sorted$indice <- seq(from = 1, to = nrow(mean_vals_sorted))
-# # rownames(mean_vals_sorted) <- seq(from = 1, to = nrow(mean_vals_sorted))
-# 
-# # crea un vector con los indices ordenados
-# numeros_asignados <- mean_vals_sorted$indice
-# 
-# # cambia los valores de las categorias por los indices ordenados de mayor a menor
-# # poblacion3$new_vals <- numeros_asignados[match(poblacion$loan, mean_vals_sorted$Group.1)]
-# poblacion3$loan <- numeros_asignados[match(poblacion3$loan, mean_vals_sorted$Group.1)]
-
-# for(i in seq_along(poblacion3$loan)) {
-#   # Aquí va el código que queremos ejecutar para cada índice i
-#   print(paste("Fila", i, ": loan =", poblacion3$loan[i], ", new_vals =", poblacion3$new_vals[i]))
-# }
-
-# poblacion3 <- poblacion2
-# assign_numerical_values(poblacion3, "loan", "y")
-# poblacion3[, "loan"]
+# Aca analice la tendencia, si las tasas se parecen debo sumarla a otra vareiable que se parezca más 
+# y tenga lógica y tambien  causalidad (eso se ve con el el step wize)
 
 ########################################################################################################
 #         NUEVA FUNCION PARA CAMBIAR CATEGORICAS POR NUMERICAS
@@ -367,7 +364,9 @@ assign_numerical_values1 <- function(data, categorical_var,xd, numerical_var){
   return(xd)
 }
 
-# COMANDOS PARA PROBAR
+str(poblacion2)
+
+# 3era copia de l base de datos
 poblacion3 <- poblacion2
 
 #Pasar variables Y, pasarla a binario
@@ -383,49 +382,162 @@ poblacion3$education <- assign_numerical_values1(poblacion3, "education",poblaci
 poblacion3$default <- assign_numerical_values1(poblacion3, "default",poblacion3$default, "y")
 # caso de housing 
 poblacion3$housing <- assign_numerical_values1(poblacion3, "housing",poblacion3$housing, "y")
+# caso de loan 
+poblacion3$loan <- assign_numerical_values1(poblacion3, "loan",poblacion3$loan, "y")
 # caso de contact 
 poblacion3$contact <- assign_numerical_values1(poblacion3, "contact",poblacion3$contact, "y")
 # caso de month 
+poblacion3$month <- assign_numerical_values1(poblacion3, "month",poblacion3$month, "y")
+# caso de day_of_week 
 poblacion3$day_of_week <- assign_numerical_values1(poblacion3, "day_of_week",poblacion3$day_of_week, "y")
 # caso de poutcome 
 poblacion3$poutcome <- assign_numerical_values1(poblacion3, "poutcome",poblacion3$poutcome, "y")
 
-
-
 # Pasar variables chr a int
 poblacion3$emp.var.rate <- as.integer(poblacion3$emp.var.rate)
 poblacion3$cons.price.idx <- as.integer(poblacion3$cons.price.idx)
-poblacion3$cons.price.idx <- as.integer(poblacion3$cons.price.idx)
+poblacion3$cons.conf.idx <- as.integer(poblacion3$cons.conf.idx)
+poblacion3$euribor3m <- as.integer(poblacion3$euribor3m)
 poblacion3$nr.employed <- as.integer(poblacion3$nr.employed)
 
+# NOTA: SON TODOS CATEÓRICOS
+unknown_count <- colSums(poblacion3 == 'unknown')
+
+###########################################################################
+###########################################################################
+
+# analizar la correlacion
+str(poblacion3)
 
 
-##############################################################################################################################
-##############################################################################################################################
-##############################################################################################################################
+# Calcular la matriz de correlación
+cor_matrix <- cor(poblacion3)
+
+# Crear un filtro para seleccionar las correlaciones mayores al 80%
+cor_filter <- cor_matrix > 0.8
+
+# Seleccionar las variables que cumplen el filtro
+selected_vars <- which(cor_filter, arr.ind = TRUE)
+
+# Imprimir las variables seleccionadas y sus respectivas correlaciones
+for (i in 1:nrow(selected_vars)) {
+  var1 <- rownames(selected_vars)[selected_vars[i, 1]]
+  var2 <- rownames(selected_vars)[selected_vars[i, 2]]
+  corr <- cor_matrix[selected_vars[i, 1], selected_vars[i, 2]]
+  cat(sprintf("%s y %s tienen una correlación de %.2f\n", var1, var2, corr))
+}
 
 
-# Aca analice la tendencia, si las tasas se parecen debo sumarla a otra vareiable que se parezca más 
-#y tenga lógica y tambien  causalidad (eso se ve con el el step wize)
+# Eliminr la variable cons.price.idx y emp.var.rate tienen una correlación de 0.93
+# SE ELININA: cons.price.idx para evitar multicoleanilidad
 
-###############################################################
+# Crear un nuevo data frame sin la variable "cons.price.idx"
+poblacion4 <- select(poblacion3, -cons.price.idx)
 
-# Realizar group by de la variable "education" y contar la cantidad de instancias de cada categoría
-education_counts <- aggregate(poblacion2$education, by = list(education = poblacion2$education), FUN = length)
+# regresión logistica
 
-# Renombrar la variable "age" a "count"
-names(education_counts)[2] <- "count"
+# Separar conjuntos de entrenamiento y prueba .
+n <- nrow(poblacion4)
+n_entrenamiento <- floor(0.8*n)
+muestra <- sample.int(n = n, size = n_entrenamiento, replace = FALSE)
+entrenamiento <- poblacion4[muestra,]
+prueba <- poblacion4[-muestra,]
+
+set.seed(1313)
+
+# Ajustar modelo .
+#modelo <- glm(am ~ wt , family = binomial(link="logit"), data=entrenamiento)
+#print(summary(modelo))
+
+
+# Modelo con todas las variables
+modelo <- glm(y ~ ., data = entrenamiento, family = "binomial")
+summary(modelo)
+
+# Modelo con variables seleccionadas
+modelo_stepwise <- stepAIC(modelo, direction = "both", trace = FALSE)
+summary(modelo_stepwise)
+
+# Evaluar el modelo con el conjunto de entrenamiento .
+probs_e <- predict(modelo_stepwise, entrenamiento, type="response")
+
+umbral <- 0.5
+preds_e <- sapply(probs_e,function(p)ifelse(p >=umbral, "1", "0"))
+preds_e <- factor(preds_e, levels=levels(poblacion4[["y"]]))
+
+ROC_e<- roc(entrenamiento[["y"]], probs_e)
+plot(ROC_e)
+
+# Tiene un AUC de 92,72%
+
+############
+prediccion <- predict(modelo_stepwise, newdata = prueba, type = "response")
+
+tabla <- table(prueba$y, prediccion > 0.5)
+
+# La matriz de confusión nos indica cuántos registros fueron clasificados 
+# correctamente y cuántos fueron clasificados incorrectamente. En este caso,
+# la probabilidad umbral para clasificar como positivo es 0.5
+
+exactitud <- sum(diag(tabla)) / sum(tabla)
+# tiene 90,93227% de exactitud
+
+# crear matriz de confusión
+matriz_confusion <- matrix(c(7122, 188, 559, 369), nrow = 2, ncol = 2, byrow = TRUE, dimnames = list(c("0", "1"), c("0", "1")))
+
+# calcular métricas
+evaluacion <- confusionMatrix(matriz_confusion)
+
+####### Evaluar el modelo con el conjunto de prueba ######.
+probs_e <- predict(modelo_stepwise, prueba, type="response")
+
+umbral <- 0.5
+preds_e <- sapply(probs_e,function(p)ifelse(p >=umbral, "1", "0"))
+preds_e <- factor(preds_e, levels=levels(poblacion4[["y"]]))
+
+ROC_e<- roc(prueba[["y"]], probs_e)
+plot(ROC_e)
+
+# Ahora, evaluando con menos variables, es decir, sta vez solo incluyendo las variables significativas
+modelo2 <- glm(y ~ job + default + contact + month + duration + previous + poutcome + emp.var.rate + cons.conf.idx + nr.employed, family = "binomial", data = entrenamiento)
+summary(modelo2)
+
+
+# Evaluar el modelo con el conjunto de entrenamiento .
+probs_e <- predict(modelo2, entrenamiento, type="response")
+
+umbral <- 0.5
+preds_e <- sapply(probs_e,function(p)ifelse(p >=umbral, "1", "0"))
+preds_e <- factor(preds_e, levels=levels(poblacion4[["y"]]))
+
+ROC_e<- roc(entrenamiento[["y"]], probs_e)
+plot(ROC_e)
+
+prediccion <- predict(modelo_stepwise, newdata = prueba, type = "response")
+
+tabla <- table(prueba$y, prediccion > 0.5)
+
+# La matriz de confusión nos indica cuántos registros fueron clasificados 
+# correctamente y cuántos fueron clasificados incorrectamente. En este caso,
+# la probabilidad umbral para clasificar como positivo es 0.5
+
+exactitud <- sum(diag(tabla)) / sum(tabla)
+
+# Ahora con los datos de prueba
+probs_e <- predict(modelo2, prueba, type="response")
+
+umbral <- 0.5
+preds_e <- sapply(probs_e,function(p)ifelse(p >=umbral, "1", "0"))
+preds_e <- factor(preds_e, levels=levels(poblacion4[["y"]]))
+
+ROC_e<- roc(prueba[["y"]], probs_e)
+plot(ROC_e)
 
 
 
-########### Transformación de variables categóricas ###########
-
-str(poblacion2)
 
 
 
-####################################
-# APLICAR MONTECARLO Y FISHER
 
 
 
